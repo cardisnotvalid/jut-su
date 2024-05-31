31fa0b9826826bfaaca47d2aef8db796f34acb61
ByteBlunder <deadcardinal293@gmail.com>
2024-05-31 20:00:02 +0700

    Added logging

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,14 @@
 import sys
 import logging
 
-FORMAT_LOG  = "[%(levelname)s] %(message)s"
+FORMAT_LOG  = "%(levelname)s: %(message)s"
 FORMAT_DATE = "%H:%M:%S"
 
 formatter = logging.Formatter(fmt=FORMAT_LOG, datefmt=FORMAT_DATE)
 handler = logging.StreamHandler(stream=sys.stdout)
 handler.setFormatter(formatter)
 
-logger = logging.getLogger("jutsu")
+logger = logging.getLogger("log")
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
+
