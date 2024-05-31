13a8fa6a018e1a96c5c23e9f672e4b0fd7820922
ByteBlunder <deadcardinal293@gmail.com>
2024-05-31 20:02:44 +0700

    Small things

--- main.py
+++ main.py
@@ -1,62 +1,17 @@
-from collections import defaultdict
-import questionary
 from src.jutsu import JutSu
+from src.logger import logger
+from src.session import Session
 
 if __name__ == "__main__":
     jutsu = JutSu()
     try:
-        search_query = questionary.text("Поиск:", default="Ванпанчмен").ask()
-
-        search_result = jutsu.search(search_query)
-        anime_names = [item.name for item in search_result]
-
-        if not anime_names:
-            questionary.print("Ничего не нашлось").ask()
-            exit(0)
-
-        selected_anime = questionary.select("Выберите аниме:", choices=anime_names).ask()
-
-        d_anime = None
-        for anime in search_result:
-            if anime.name == selected_anime:
-                d_anime = anime
-                break
-
-        anime = jutsu.anime(d_anime)
-
-        episodes = defaultdict(list)
-        for item in anime.episodes:
-            episodes[str(item.season)].append(item.id)
-
-        selected_season = questionary.select(
-            "Выберите сезон:", choices=episodes.keys()).ask()
-        selected_episode = questionary.select(
-            "Выбрите эпизод:", choices=episodes[selected_season]).ask()
-
-        ani_epi = None
-        for item in anime.episodes:
-            if item.id == selected_episode and item.season == selected_season:
-                ani_epi = item
-                break
-
-        epi_video = jutsu.get_episode_videos(ani_epi)
-        vi_quality = [item.quality for item in epi_video]
-
-        selected_video = questionary.select(
-            "Выбрите качество видео:", choices=vi_quality).ask()
-
-        ani_vi = None
-        for item in epi_video:
-            if item.quality == selected_video:
-                ani_vi = item
-                break
-
-        print("Downloading video...")
-        with jutsu.session.get(ani_vi.url, stream=True) as r:
-            r.raise_for_status()
-            with open("video.mp4", "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        print("Done!")
+        r_search = jutsu.search("дворецкий")
+        print(r_search)
+        r_anime = jutsu.get_anime(r_search.pop(0))
+        print(r_anime)
+        r_video = jutsu.get_episode_video(r_anime.episodes.pop(0))
+        print(r_video)
+    except Exception as err:
+        logger.error("Произошла ошибка: %s", err)
     finally:
         jutsu.close()
