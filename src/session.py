6b8b0ca2be087745775bec1d2d05fa3e06466d50
Oyunsky <deadcardinal293@gmail.com>
2024-06-01 23:41:53 +0700

    Refactored

--- src/session.py
+++ src/session.py
@@ -1,14 +1,21 @@
-import time
+import os
 import urllib3
-from typing import Dict, Any
+from urllib3.response import BaseHTTPResponse
+from urllib3.exceptions import HTTPError
+from typing import Literal, Generator
 
 from .logger import logger
-from .utils import reset_file
+from .exceptions import CantGetRequestContent, ResponseStatusCodeError
 
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"}
+DEFAULT_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+
+RequestMethod = Literal["GET", "POST"]
 
 class Session:
-    def __init__(self) -> None:
+    def __init__(self, headers: dict[str, str] | None = {}) -> None:
+        self.headers = {"User-Agent": DEFAULT_USER_AGENT}
+        self.headers.update(headers)
+
         self.session = urllib3.PoolManager(headers=headers)
 
     def __enter__(self) -> "Session":
@@ -20,49 +27,52 @@ class Session:
     def close(self) -> None:
         self.session.clear()
 
-    def download_html(self, url: str, data: Dict[str, Any] = None) -> str:
+    def _request(self, method: RequestMethod, url: str, **kwargs) -> BaseHTTPResponse:
         try:
-            response = self.session.request("POST", url, fields=data)
-            if response.status == 200:
-                return response.data.decode("1251")
-            else:
-                logger.error(
-                    "Не удалось установить соединение. Статус: %d (%s)", 
-                    response.status, response.reason)
-                return False
-        except Exception as err:
-            logger.error("Не удалось получить страницу: %s", err)
-            return False
+            response = self.session.request(method, url, **kwargs)
+            self._check_response_status(response)
+            return response
+        except HTTPError:
+            raise CantGetRequestContent
+
+    def _check_response_status(self, response: BaseHTTPResponse) -> None:
+        if response.status != 200:
+            raise ResponseStatusCodeError(response.status, response.reason)
+
+    def _post(self, url: str, data: dict[str, any]) -> BaseHTTPResponse:
+        return self._request("POST", url, **{"fields": data})
+
+    def _get(self, url: str, **kwargs) -> BaseHTTPResponse:
+        return self._request("GET", url, **kwargs)
+
+    def _open_response_stream(self, response: BaseHTTPResponse) -> Generator[bytes, None, None]:
+        size_content = self._get_response_content_size(response)
+        size_download = 0.0
+        for chunk in response.stream(65535):
+            size_download += len(chunk) / (1<<20)
+            yield chunk
+            logger.info("%.2fMB / %.2fMB\033[K\033[1A", size_download, size_content)
+
+    def _get_response_content_size(self, response: BaseHTTPResponse) -> float:
+        content_length = response.getheader("Content-Length")
+        return int(content_length) / (1<<20)
+    
+    def _save_video_to_file(self, response: BaseHTTPResponse, filepath: str) -> None:
+        if os.path.isfile(filepath):
+            with open(filepath, "wb") as file:
+                file.truncate(0)
+
+        with open(filepath, "ab") as f:
+            for chunk in self._open_response_stream(response):
+                f.write(chunk)
+
+    def get_html_content(self, url: str, data: dict[str, any] | None = None) -> str:
+        response = self._post(url, data) if data else self._get(url)
+        return response.data.decode("1251")
 
     def download_video(self, url: str, filepath: str) -> None:
         try:
-            response = self.session.request("GET", url, preload_content=False)
-            if response.status != 200:
-                logger.error(
-                    "Не удалось установить соединение. Статус: %d (%s)", 
-                    response.status, response.reason)
-                return False
-
-            content_length = response.getheader("Content-Length")
-            c_size = int(content_length) / (1<<20)
-            d_size = 0.0
-
-            reset_file(filepath)
-
-            with open(filepath, "ab") as file:
-                start_time = time.perf_counter()
-                for chunk in response.stream(65535):
-                    if chunk:
-                        file.write(chunk)
-                        d_size += len(chunk) / (1<<20)
-                        elapsed_time = time.perf_counter() - start_time
-                        speed = d_size / elapsed_time if elapsed_time > 0 else 0
-                        logger.info(
-                            "%.2fMB / %.2fMB [%.2fMB/s]\033[K\033[1A",
-                            d_size, c_size, speed)
-            return True
-        except Exception as err:
-            logger.error("Не удалось скачать видео: %s", err)
-            return False
+            response = self._get(url, **{"preload_content": False})
+            self._save_video_to_file(response, filepath)
         finally:
             response.release_conn()
