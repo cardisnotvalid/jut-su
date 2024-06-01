a1cc363e7a9bc1a5ab935352877b44985b6bfc81
Oyunsky <deadcardinal293@gmail.com>
2024-06-02 06:37:45 +0700

    Refactored under the 'TypedDict' models

--- main.py
+++ main.py
@@ -1,106 +1,90 @@
-from typing import List
-from collections import defaultdict
+from urllib.parse import urlparse
 
 import questionary
+from questionary import Choice
 
+from src.paths import DIR_OUTPUT
 from src.session import Session
-from src.jutsu import JutSu, Anime, Episode, Video
+from src.jutsu import JutSu, Anime, Season, Video
 from src.logger import logger
-from src.utils import create_anime_dir
 
-def quest_search(jutsu: JutSu) -> Anime:
+
+def quest_search(jutsu: JutSu) -> list[Anime]:
     search_answer = questionary.text("Поиск:").ask()
     if not search_answer:
         questionary.print("Вы ничего не ввели. Введите название аниме")
         return quest_search(jutsu)
-
     if search_answer == "q":
         exit(0)
-
-    r_search = jutsu.search(search_answer)
-    if not r_search:
-        if r_search is False:
-            questionary.print("Произошла ошибка")
-            exit(1)
-        else:
-            questionary.print("Ничего не найдено")
-
-        action_answer = questionary.select(
+    search_result = jutsu.search(search_answer)
+    if not search_result:
+        questionary.print("Ничего не найдено")
+        action = questionary.select(
             "Действие:", 
-            choices=["Поиск", "Выйти"]).ask()
-        if action_answer == "Поиск":
+            choices=["Поиск", "Выйти"]
+        ).ask()
+        if action == "Поиск":
             return quest_search(jutsu)
         else:
             exit(0)
+    return search_result
 
-    return r_search
 
-def quest_anime(jutsu, data: List[Anime]) -> Anime:
-    if not data:
-        exit(1)
+def quest_anime(jutsu, animes: list[Anime]) -> Anime:
+    anime = questionary.select(
+        "Выбрите аниме:", 
+        choices=[Choice(item["name"], item) for item in animes]
+    ).ask()
+    return jutsu.get_anime(anime)
 
-    choice_list = [item.name for item in data]
-    anime_answer = questionary.select(
-        "Выбрите аниме:",
-        choices=choice_list).ask()
 
-    target = next(item for item in data if item.name == anime_answer)
-    return jutsu.get_anime(target)
-
-def quest_episode(jutsu, data: Anime):
-    if not data:
-        exit(1)
-
-    choice_list = defaultdict(list)
-    for item in data.episodes:
-        choice_list[item.season].append(item.id)
-
-    season_answer = questionary.select(
+def quest_episode(jutsu, anime: Anime) -> tuple[str, str]:
+    season_id = questionary.select(
         "Выберите сезон:",
-        choices=choice_list).ask()
-
-    episode_answer = questionary.select(
+        choices=anime["episodes"]
+    ).ask()
+    episode_url = questionary.select(
         "Выберите эпизод:",
-        choices=choice_list[season_answer]).ask()
+        choices=[
+            Choice(str(i+1), anime["episodes"][season_id][i]) 
+            for i in range(len(anime["episodes"][season_id]))
+        ]
+    ).ask()
+    return (season_id, episode_url)
 
-    return next((
-        item for item in data.episodes 
-        if item.id == episode_answer and
-            item.season == season_answer), None)
 
-def quest_video(jutsu: JutSu, data: Episode) -> Video:
-    if not data:
-        exit(1)
-
-    r_video = jutsu.get_episode_video(data)
-    if not r_video:
-        logger.error("Не удалось получить видео")
-        exit(1)
-
-    choice_list = [item.quality for item in r_video]
-    quality_answer = questionary.select(
+def quest_video(jutsu: JutSu, url: str) -> str:
+    vidoes = jutsu.get_episode_videos(url)
+    return questionary.select(
         "Выберите качество видео:",
-        choices=choice_list).ask()
+        choices=[Choice(video.quality, video.url) for video in vidoes]
+    ).ask()
 
-    return next((
-        item for item in r_video 
-        if item.quality == quality_answer), None)
 
 if __name__ == "__main__":
     jutsu = JutSu()
     try:
-        r_search = quest_search(jutsu)
-        r_anime = quest_anime(jutsu, r_search)
-        r_episode = quest_episode(jutsu, r_anime)
-        r_video = quest_video(jutsu, r_episode)
+        finded_animes = quest_search(jutsu)
+        anime = quest_anime(jutsu, finded_animes)
+        (season_id, episode_url) = quest_episode(jutsu, anime)
+        video_url = quest_video(jutsu, episode_url)
 
-        dir_path = create_anime_dir(r_anime.name, r_episode.season)
-        file_name = f"{r_episode.id}.{r_video.ext}"
-        file_path = dir_path / file_name
+        dir_anime = DIR_OUTPUT / anime["name"]
+        dir_season = dir_anime / f"Сезон {season_id}"
+        dir_season.mkdir(parents=True, exist_ok=True)
 
-        jutsu.session.download_video(r_video.url, file_path)
-        logger.info("%s. Сезон %s. Эпизод %s успешно скачалось", 
-                    r_anime.name, r_episode.season, r_episode.id)
+        parsed_video_url = urlparse(video_url)
+        url_info = parsed_video_url.path.rsplit("/", 1)[-1]
+        episode_id, *_, video_ext = url_info.split(".")
+
+        file_name = f"{episode_id}.{video_ext}"
+        file_path = dir_season / file_name
+
+        jutsu.session.download_video(video_url, file_path)
+        logger.info(
+            "%s. Сезон %s. Эпизод %s успешно скачалось", 
+            anime["name"], season_id, file_name
+        )
     finally:
         jutsu.close()
 
