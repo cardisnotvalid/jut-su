70088f7c7fd1acb94e38fc09a19f7a5cbd5a1d6d
Oyunsky <deadcardinal293@gmail.com>
2024-06-02 06:36:17 +0700

    Deleted file `utils.py`

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-from .paths import DIR_OUTPUT
-
-def create_anime_dir(name: str, season: str | int) -> str:
-    name_dir = DIR_OUTPUT / name
-    season_dir = name_dir / f"Сезон {season}"
-    season_dir.mkdir(parents=True, exist_ok=True)
-    return season_dir
-
