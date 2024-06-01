1ed4ecddcfdccc8c3a7b925916b2cf5f02750367
Oyunsky <deadcardinal293@gmail.com>
2024-06-02 02:26:52 +0700

    Deleted models

--- src/models.py
+++
@@ -1,8 +0,0 @@
-__all__ = ["Search", "Video", "Anime", "Episode"]
-
-from collections import namedtuple
-
-Search = namedtuple("Search", ["name", "url"])
-Video = namedtuple("Video", ["quality", "ext", "url"])
-Anime = namedtuple("Anime", ["name", "episodes", "url"])
-Episode = namedtuple("Episode", ["id", "season", "url"])
