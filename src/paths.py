8db37cbbec8538f2f999d74b75e9201272c2a40c
Oyunsky <deadcardinal293@gmail.com>
2024-06-04 02:58:44 +0700

    Added file formatting functions

--- src/paths.py
+++ src/paths.py
@@ -1,10 +1,21 @@
-from pathlib import Path
+import os
+from urllib.parse import urlparse
 
 
 URL_BASE = "https://jut.su"
 URL_ANIME = f"{URL_BASE}/anime"
 
-DIR_BASE = Path(__file__).resolve().parent.parent
-DIR_OUTPUT = DIR_BASE / "output"
+DIR_BASE = os.getcwd()
+DIR_OUTPUT = os.path.join(DIR_BASE, "output")
 
-DIR_OUTPUT.mkdir(exist_ok=True)
+os.makedirs(DIR_OUTPUT, exist_ok=True)
+
+
+def get_video_filename(url):
+    parsed_url = urlparse(url)
+    (video_id, *_, video_ext) = parsed_url.path.rsplit("/", 1)[-1].split(".")
+    return f"{video_id}.{video_ext}"
+
+
+def get_output_filepath(filename):
+    return os.path.join(DIR_OUTPUT, filename)
